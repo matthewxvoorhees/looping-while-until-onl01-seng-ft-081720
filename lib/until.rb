f5d40239d900d32a376b3048f179e25ffd717720
Matthew Crnich <matthew.crnich@gmail.com>
2020-08-18 19:13:58 +0000

    Automatically backed up by Learn

--- lib/until.rb
+++ lib/until.rb
@@ -1,8 +1,13 @@
 
-
 def using_until
   levitation_force = 6
-  
+  until levitation_force == 10
+    puts   
     
 end
 
+counter = 0
+until counter == 20
+  puts "The current number is less than 20."
+  counter += 1
+end
