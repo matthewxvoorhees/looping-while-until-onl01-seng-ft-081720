8f7a09fe7a6dcf557ce85a6ec8167f7b689f608c
Matthew Crnich <matthew.crnich@gmail.com>
2020-08-18 19:11:29 +0000

    Automatically backed up by Learn

--- lib/while.rb
+++ lib/while.rb
@@ -3,11 +3,3 @@ while levitation_force < 10
   puts "Wingardium Leviosa"
   levitation_force += 1
 end
-
-
-counter = 0
-while counter < 20
-  puts "The current number is less than 20."
-  counter += 1
-end
-
